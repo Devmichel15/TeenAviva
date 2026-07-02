c3196702d26bb028971884040eada28dc1f22892
Michel César <httpmichel15cesar@gmail.com>
2026-07-02 21:03:30 +0100

    resolvendo problemas de expo router

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -4,7 +4,7 @@ import { useFonts } from "expo-font";
 export default function RootLayout() {
   const [loaded] = useFonts({
     ManropeRegular: require("../assets/fonts/Manrope-Regular.ttf"),
-    ManropeMedium: require("../assets/fonts/Manrope-Light.ttf"),
+    ManropeLight: require("../assets/fonts/Manrope-Light.ttf"),
     ManropeSemiBold: require("../assets/fonts/Manrope-SemiBold.ttf"),
     ManropeBold: require("../assets/fonts/Manrope-Bold.ttf"),
   });
--- app/index.jsx
+++ app/index.jsx
@@ -50,7 +50,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "space-between",
-    alignItems: "start",
+    alignItems: "flex-start",
     paddingTop: 100,
     paddingHorizontal: 20,
   },
